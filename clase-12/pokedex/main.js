f316355c12218770cc5cb44097fca06aaa60418c
k- <mahat.mejor@gmail.com>
2020-04-26 12:22:51 -0500

    fix errors

--- clase-12/pokedex/main.js
+++ clase-12/pokedex/main.js
@@ -1,98 +1,95 @@
-const pokemonContainer = document.querySelector(".pokemon");
-const pokemonName = pokemonContainer.querySelector(".pokemonName");
-const pokemonImage = pokemonContainer.querySelector(".pokemonImg");
-const pokemonWeight = pokemonContainer.querySelector(".pokemonWeight");
-const pokemonHeight = pokemonContainer.querySelector(".pokemonHeight");
-const pokemonTypes = pokemonContainer.querySelector(".pokemonTypes");
-const pokemonChangeImages = pokemonContainer.querySelector(".pokemon-images");
-const nextPokemon = document.querySelector(".nextPokemon");
-const previousPokemon = document.querySelector(".previousPokemon");
-const pokemonList = document.querySelector(".pokemonList");
-
+const pokemonContainer = document.querySelector('.pokemon');
+const pokemonName = pokemonContainer.querySelector('.pokemonName');
+const pokemonImage = pokemonContainer.querySelector('.pokemonImg');
+const pokemonWeight = pokemonContainer.querySelector('.pokemonWeight');
+const pokemonHeight = pokemonContainer.querySelector('.pokemonHeight');
+const pokemonTypes = pokemonContainer.querySelector('.pokemonTypes');
+const pokemonChangeImages = pokemonContainer.querySelector('.pokemon-images');
+const nextPokemonHandler = document.querySelector('.nextPokemon');
+const previousPokemonHandler = document.querySelector('.previousPokemon');
+const pokemonList = document.querySelector('.pokemonList');
+let pokemonImgs = [];
 let id = 1;
 
-getPokemon(id);
-function getPokemon(id) {
+function init() {
+    getPokemon(id);
+    getPokemonList();
+    nextPokemonHandler.addEventListener('click', () => {
+        id++;
+        removeTypes();
+        getPokemon(id);
+    });
+    previousPokemonHandler.addEventListener('click', () => {
+        id--;
+        removeTypes();
+        getPokemon(id);
+    });
+}
+
+function getPokemon(id = 1) {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-        fetch(url)
+    fetch(url)
+        .then((response) => response.json())
+        .then((response) => showPokemon(response));
+}
+
+function getPokemonList() {
+    const url = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=800';
+    fetch(url)
         .then(function (response) {
             return response.json();
         })
-        .then((response)=>showPokemon(response));
+        .then((response) => populateList(response.results));
 }
 
-getPokemonList();
-function getPokemonList(){
-    const url = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=800";
-    fetch(url)
-    .then(function (response) {
-        return response.json();
-    })
-    .then((response)=>populateList(response.results));
-}
-
-function populateList(pokemones){
-    pokemones.forEach((pokemon,i)=>{
-        const p = document.createElement("p");
+function populateList(pokemones) {
+    pokemones.forEach((pokemon, i) => {
+        const p = document.createElement('p');
         p.innerHTML = pokemon.name;
-        (function(i){
-            p.addEventListener("click",()=>{
+        (function () {
+            p.addEventListener('click', () => {
                 removeTypes();
-                getPokemon(i+1)
+                getPokemon(i + 1);
             });
         })(i);
         pokemonList.appendChild(p);
     });
 }
 
-let pokemonImgs = []
-
-function changeImages(){
+function changeImages() {
     removeImageButtons();
-    let validImages = Object.entries(pokemonImgs)
-        .filter(a=>{
-        if(a[1]!=null){
-            return a[1]
-        }
-    })
-    validImages.forEach(e=>{
-        const button = document.createElement("button");
-        button.addEventListener("click",()=>{
+    let validImages = Object.entries(pokemonImgs).filter((a) => {
+        return a[1] || undefined;
+    });
+    validImages.forEach((e) => {
+        const button = document.createElement('button');
+        button.addEventListener('click', () => {
             pokemonImage.src = e[1];
         });
         pokemonChangeImages.appendChild(button);
     });
 }
 
-function showPokemon(response){
+function showPokemon(response) {
     pokemonName.innerHTML = response.name;
     pokemonImgs = response.sprites;
     pokemonImage.src = response.sprites.front_default;
     pokemonHeight.innerHTML = `Height : ${response.height}`;
     pokemonWeight.innerHTML = `Weight : ${response.weight}`;
-    response.types.forEach(e => {
-        let span = document.createElement("span");
+    response.types.forEach((e) => {
+        let span = document.createElement('span');
         span.innerText = e.type.name;
         pokemonTypes.appendChild(span);
     });
     changeImages();
 }
 
-nextPokemon.addEventListener("click",()=>{
-    id++;
-    removeTypes();
-    getPokemon(id);
-});
-previousPokemon.addEventListener("click",()=>{
-    id--;
-    removeTypes();
-    getPokemon(id);
-});
-
-function removeTypes(){
-    pokemonTypes.innerHTML = "";
+function removeTypes() {
+    pokemonTypes.innerHTML = '';
 }
 
-function removeImageButtons(){
-    pokemonChangeImages.innerHTML = "";
+function removeImageButtons() {
+    pokemonChangeImages.innerHTML = '';
 }
+
+init();
